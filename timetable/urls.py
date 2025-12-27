d4f5c2939129f054e00f30315b1056e08fc2ed18
zafrulhuzail <huzailakazaf@gmail.com>
2025-12-28 00:08:11 +0100

    add day_view

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import student_timetable, my_timetable
+from .views import student_timetable, my_timetable, day_view
 
 urlpatterns = [
     path("student/<int:student_id>/timetable/", student_timetable, name="student_timetable"),
     path("me/", my_timetable, name="my_timetable"),   
+    path("me/day_view/", day_view, name="day_view"),   
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .services import get_student_timetable
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 # Create your views here.
 
 WEEKDAY_NAMES = {
@@ -36,4 +37,26 @@ def my_timetable(request):
         'student_first_name': student.first_name,
         'student_last_name': student.last_name,
         'items': items,
+    })
+
+@login_required
+def day_view(request):
+    user = request.user
+    current_day = datetime.now().weekday()
+
+    if not hasattr(user, 'student'): 
+        return HttpResponse("You are not a student.", status=403)
+    
+    student = user.student
+    items = get_student_timetable(student.student_id)
+
+    day_items = []
+    for item in items:
+        if item.weekday == WEEKDAY_NAMES[current_day]:
+            day_items.append(item)
+
+    return render(request, 'timetable/student_timetable.html', {
+        'student_first_name': student.first_name,
+        'student_last_name': student.last_name,
+        'items': day_items,
     })
