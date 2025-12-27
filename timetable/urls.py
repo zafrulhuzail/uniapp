b684e8a124d0c8e343c4556df29b0af08af380d2
zafrulhuzail <huzailakazaf@gmail.com>
2025-12-27 23:09:37 +0100

    add login redirect to timetable with private url

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import student_timetable
+from .views import student_timetable, my_timetable
 
 urlpatterns = [
-    path("student/<int:student_id>/timetable/", student_timetable, name="student_timetable"),   
+    path("student/<int:student_id>/timetable/", student_timetable, name="student_timetable"),
+    path("me/", my_timetable, name="my_timetable"),   
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .services import get_student_timetable
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 WEEKDAY_NAMES = {
@@ -18,5 +19,21 @@ def student_timetable(request, student_id):
   items = get_student_timetable(student_id)
   return render(request, 'timetable/student_timetable.html', {
         student_id: student_id,
+        'items': items,
+    })
+
+@login_required
+def my_timetable(request):
+    user = request.user
+
+    if not hasattr(user, 'student'):
+        return HttpResponse("You are not a student.", status=403)
+
+    student = user.student
+    items = get_student_timetable(student.student_id)
+
+    return render(request, 'timetable/student_timetable.html', {
+        'student_first_name': student.first_name,
+        'student_last_name': student.last_name,
         'items': items,
     })
