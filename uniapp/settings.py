8d6effb81c25a9f8dd9291ee2561bd7d30c5a0da
zafrulhuzail <huzailakazaf@gmail.com>
2025-12-27 23:05:43 +0100

    add django login

--- uniapp/settings.py
+++ uniapp/settings.py
@@ -126,5 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
--- uniapp/urls.py
+++ uniapp/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('courses/', include('courses.urls')),
     path("timetable/", include("timetable.urls")),
 ]
