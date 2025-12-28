6f5c3ea8c454f09d17f87afd6c19dd9b26cb87bf
zafrulhuzail <huzailakazaf@gmail.com>
2025-12-28 14:15:54 +0100

    add longer jwt lifespan

--- uniapp/settings.py
+++ uniapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'buildings',
     'timetable',
     'rest_framework',
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 REST_FRAMEWORK = {
@@ -51,6 +53,17 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+}
+
+SIMPLE_JWT = {
+   
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+
+    "ROTATE_REFRESH_TOKENS": True,      
+    "BLACKLIST_AFTER_ROTATION": True,   
+    "UPDATE_LAST_LOGIN": True,
 }
 
 MIDDLEWARE = [
